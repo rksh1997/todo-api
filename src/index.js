5697ff1fc2a24d90c2ec6f0dcbe432835ce1aa0b
Rashad Kokash <richardeo112@gmail.com>
2019-12-03 17:39:00 +0200

    Wrap main() in index.js with try catch

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import winston from './lib/winston';
 import { PORT } from './config';
 
 async function main() {
-  await connectDB();
-  winston.info('Connected to database');
-  await app.listen(PORT);
-  winston.info(`API is live on: http://localhost:${PORT}`);
+  try {
+    await connectDB();
+    winston.info('Connected to database');
+    await app.listen(PORT);
+    winston.info(`API is live on: http://localhost:${PORT}`);
+  } catch (e) {
+    winston.error(e);
+  }
 }
 
 main();
